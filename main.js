6c1050ac8f1e7bb98ef4d07ae7b01a2401f1a91c
Parish Neighbors <pn8bors@gmail.com>
2022-02-03 10:22:12 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import input from 'input';
+
+function clean(resp) {
+    const yourResp = ['yes','yup','affirmative','definately','yeppers','yea','y',''];
+
+    if (yourResp.includes(resp)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+async function main() {
+    const resp = await input.text('Do you want to finish?');
+    const isFinished = clean(resp);
+
+    if (isFinished) {
+        console.log('Done');
+    } else {
+        console.log('Not done');
+    }
+}
+
+main();
